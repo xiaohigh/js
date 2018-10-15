7539856924fa46b446e521b6234504cc0b803c1c
Jeremiah Faria <jmfcool@me.com>
2018-10-14 19:55:25 -0700

    Add bootstrap to Author Quiz and add props

---
+++ _apps/_react/quiz-author/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>Author Quiz</div>
+    );
+  }
+}
+
+export default App;
