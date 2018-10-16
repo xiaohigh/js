0c7420ce1c3278d3c3727978f6b637e87965b41a
Jeremiah Faria <jmfcool@me.com>
2018-10-16 03:20:10 -0700

    Clean up

--- _apps/_react/quiz-author/src/App.js
+++ _apps/_react/quiz-author/src/App.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import '../../../bootstrap.min.css';
+import './bootstrap.min.css';
 
-class App extends Component {
-  render() {
-    return (
-        <div>Author Quiz</div>
-    );
-  }
+const Hero = () => {
+    return (<div className="row">
+                <div className="jumbotron col-10 offset-1">
+                    <h1>Author Quiz</h1>
+                    <p>Select the book by the author shown</p>
+                </div>
+            </div>);
 }
 
-export default App;
+const Book = ({title, onClick}) => {
+    return (<div className="answer" onClick={() => {onClick(title);}}>
+                <h4>{title}</h4>
+            </div>);
+}
+
+const Turn = ({author, books, highlight, onAnswerSelected}) => {
+    function highlightToBgColor(highlight) {
+        const mapping = {
+            none: '',
+            correct: 'green',
+            wrong: 'red'
+        };
+        return mapping[highlight];
+    }
+
+
+    return (<div className="row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
+                <div className="col-4 offset-1">
+                    <img src={author.imageUrl} className="author-image" alt="Author" />
+                </div>
+                <div className="col-6">
+                    {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
+                </div>
+            </div>);
+}
+
+const Continue = () => { return null }
+
+const Footer = () => {
+    return (<div id="footer" className="row">
+                <div className="col-12">
+                    <p className="text-muted credit">All images are from Wikimedia Commons and are in the public domain.</p>
+                </div>
+            </div>);
+}
+
+const AuthorQuiz = ({turnData, highlight, onAnswerSelected}) => {
+    return (
+        <div className="container-fluid">
+            <Hero />
+            <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+            <Continue />
+            <Footer />
+        </div>
+    );
+}
+
+export default AuthorQuiz;
--- _apps/_react/quiz-author/src/index.js
+++ _apps/_react/quiz-author/src/index.js
@@ -3,8 +3,75 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: '/images/marktwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Huckleberry Finn','Life on the Mississippi','Roughing It']
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: '/images/josephconrad.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Heart of Darkness']
+    },
+    {
+        name: 'J.K. Rowling',
+        imageUrl: '/images/jkrowling.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: '/images/stephenking.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Shining','IT']
+    },
+    {
+        name: 'Charles Dickens',
+        imageUrl: '/images/charlesdickens.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['David Copperfield','A Tale of Two Cities']
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: '/images/williamshakespeare.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Hamlet','Macbeth','Romeo and Juliet']
+    },
+];
+
+const getTurnData = (authors) => {
+    const allBooks = authors.reduce((p,c,i) => {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0,4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => author.books.some((title) => title === answer))
+    }
+}
+
+const state = {
+    turnData: getTurnData(authors),
+    highlight: 'correct'
+}
+
+function onAnswerSelected(answer) {
+    const isCorrect = state.turnData.author.books.some((book) => book === answer);
+    state.highlight = isCorrect ? 'correct' : 'wrong';
+    render();
+}
+
+function render() {
+    ReactDOM.render(<App {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+}
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
