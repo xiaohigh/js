b185f0e22ad3d987734fe8891964849130bf55ab
Jeremiah Faria <jmfcool@me.com>
2018-10-17 15:25:23 -0700

    Delete node repo

--- node/lib/internal/test/binding.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-process.emitWarning(
-  'These APIs are exposed only for testing and are not ' +
-  'tracked by any versioning system or deprecation process.',
-  'internal/test/binding');
-
-module.exports = { internalBinding };
--- node/test/parallel/test-timers-now.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-// Flags: --expose-internals
-
-require('../common');
-const assert = require('assert');
-const { getLibuvNow } = require('internal/timers');
-
-// Return value of getLibuvNow() should easily fit in a SMI after start-up.
-assert(getLibuvNow() < 0x3ffffff);
