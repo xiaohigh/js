4a712de0d273221a603c2c5ca565bb22da222b8b
Jeremiah Faria <jmfcool@me.com>
2018-11-11 16:58:23 -0800

    Update mvc

--- exercises/nodejs/modules/1-define-use/3-wrapper.js
+++ exercises/nodejs/modules/1-define-use/3-wrapper.js
@@ -1,1 +1,14 @@
-console.log(arguments);
+// function (exports, module, require, __filename, __dirname) {
+
+//let g = 1;
+
+// Node adds a function wrapper to each object.
+
+//console.log(arguments);
+
+exports.a = 42;
+module.exports.b = 37;
+
+// return module.exports
+
+// }
