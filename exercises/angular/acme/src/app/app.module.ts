c1242fa6924a7b71cf56638aed44d27c06edc45a
Jeremiah Faria <jmfcool@me.com>
2018-11-21 23:46:02 -0800

    Add acme

---
+++ exercises/angular/acme/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+
+@NgModule({
+  declarations: [
+  	AppComponent,
+  	ProductListComponent,
+  	ConvertToSpacesPipe,
+  	StarComponent
+  ],
+  imports: [
+  	BrowserModule,
+  	FormsModule,
+  	HttpClientModule
+  ],
+  bootstrap: [
+  	AppComponent
+  ]
+})
+
+export class AppModule {}
