0319377a4bd265e6482b108b3e1c732e57cac9ff
Jeremiah Faria <jmfcool@me.com>
2018-11-28 22:51:07 -0800

    Add acme product detail and worker exercise

---
+++ exercises/random/worker/index.js
@@ -0,0 +1,30 @@
+const createWorker = () => 
+{
+	let workCount = 0;
+	
+	const task1 = () => 
+	{
+		workCount += 1;
+		console.log('task1 ' + workCount);
+	};
+
+	const task2 = () => 
+	{
+		workCount += 1;
+		console.log('task2 ' + workCount);
+	};
+
+	return {
+		job1: task1,
+		job2: task2
+	};
+
+};
+
+const worker = createWorker();
+
+worker.job1();
+worker.job2();
+worker.job2();
+worker.job2();
+worker.job2();
